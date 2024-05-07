d72cfa2321cf882163ac6be330c96d5e5765c49b
Guilherme-Neves1 <guilherme.neves20@hotmail.com>
2024-05-07 08:29:29 -0300

    Aula02

---
+++ Aula02_N1/src/aula2/Cao.java
@@ -0,0 +1,19 @@
+package aula2;
+
+public class Cao {
+	public float altura, peso;
+	public String comida, nome;
+	
+	public void Correr() {
+		System.out.print("Correr \n");
+	}
+	
+	public void Latir() {
+		System.out.print("Latir \n");
+	}
+	
+	public void Morder() {
+		System.out.print("Morder \n");
+	}
+	
+}
---
+++ Aula02_N1/src/aula2/Principal.java
@@ -0,0 +1,21 @@
+package aula2;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cao cachorro1 = new Cao();
+		
+		cachorro1.peso = 5;
+		cachorro1.altura = 0.15f;
+		cachorro1.comida = "Ração";
+		cachorro1.nome = "Ruby";
+		
+		cachorro1.Latir();
+		cachorro1.Morder();
+		cachorro1.Correr();
+		
+		
+	}
+
+}
